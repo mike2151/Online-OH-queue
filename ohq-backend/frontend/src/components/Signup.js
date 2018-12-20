57e871a5c4e15a9c958230f1386e82f99dcaae4d
Mike Abelar <redefiningproductions@gmail.com>
2018-12-19 23:04:52 -0500

    create react files

---
+++ ohq-backend/frontend/src/components/Signup.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class SignUpForm extends React.Component {
+    constructor() {
+      super();
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleSubmit(event) {
+      event.preventDefault();
+      const data = new FormData(event.target);
+      
+      fetch('/api/v1/users/register/', {
+        method: 'POST',
+        body: data,
+      });
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="email">Enter Penn Email</label>
+          <input id="email" name="email" type="email" />
+  
+          <label htmlFor="first_name">First Name</label>
+          <input id="first_name" name="first_name" type="text" />
+  
+          <label htmlFor="last_name">Last Name</label>
+          <input id="last_name" name="last_name" type="text" />
+
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" />
+  
+          <button>Sign Up</button>
+        </form>
+      );
+    }
+  }
+  
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<App />, wrapper) : null;
--- ohq-backend/frontend/views.py
+++ ohq-backend/frontend/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-def index(request):
+def home(request):
     return render(request, 'frontend/home.html')
+
+def signup(request):
+    return render(request, 'frontend/signup.html')
