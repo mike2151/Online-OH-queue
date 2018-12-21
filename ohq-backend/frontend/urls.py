bcc18f377eb2c4e171d0ed7f5fd6cc767b8357b9
Mike Abelar <redefiningproductions@gmail.com>
2018-12-21 07:50:10 -0500

    multiple components

--- ohq-backend/frontend/urls.py
+++ ohq-backend/frontend/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
+    path('signup/', TemplateView.as_view(template_name='index.html')),
 ]
