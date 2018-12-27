204b8166a0c60814bdcf0810900df0378a61d30f
Arjun Guru <arjun.babolat@gmail.com>
2018-12-27 11:23:19 -0700

    Added logout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import LandingPage from "./landing-page/LandingPage";
 import QueueList from "./queues/QueueList";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/style.css"
+
 
 class App extends Component {
 
@@ -11,6 +14,8 @@ class App extends Component {
       isLoggedIn: false,
       queues: []
     };
+
+    this.logout = this.logout.bind(this);
   }
 
   componentDidMount() {
@@ -33,10 +38,19 @@ class App extends Component {
     });
   }
 
+  logout() {
+    localStorage.removeItem('credentials');
+    this.props.history.push('/login');
+  }
+
   render() {
     if(this.state.isLoggedIn) {
       return (
-       <QueueList queues={this.state.queues} />
+        <div>
+          <p>It's lit</p>
+          <button className="btn btn-primary btn-margin btn-xl" onClick={this.logout}>Log out</button>
+          <QueueList queues={this.state.queues} />
+        </div>
       );
     } else {
       return (
