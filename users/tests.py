4ac7d242b675f0cc384728cba8d48a4b324d9050
Mike Abelar <redefiningproductions@gmail.com>
2018-12-28 18:46:05 -0500

    added a test case

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import StudentUser
 
-# Create your tests here.
+class UserCreationTest(TestCase):
+    def setUp(self):
+        StudentUser.objects.create(email="test@upenn.edu", first_name="tester", last_name="smith", password="yodogyoo")
+
+    def test_user_is_created(self):
+        user = StudentUser.objects.get(email="test@upenn.edu")
+        self.assertFalse(user.is_ta)
+        self.assertEqual(user.first_name, "tester")
