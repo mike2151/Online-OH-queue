1e880f5209c8e2424d7686789b0ecce96ac11d3b
Mike Abelar <redefiningproductions@gmail.com>
2018-12-31 16:10:58 -0500

    readying for production

--- ohq/settings.py
+++ ohq/settings.py
@@ -105,6 +105,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ohq.urls'
