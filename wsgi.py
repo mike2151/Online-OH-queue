1fa38e279f2427e4e7b7ccef2bda7276f9c73b35
Mike Abelar <redefiningproductions@gmail.com>
2018-12-31 16:03:42 -0500

    readying for production

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 import os
-from django.core.wsgi
-import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reactdjango.settings")
