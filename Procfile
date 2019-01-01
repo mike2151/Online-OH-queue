64cb83380596ef888fefa8a3c2a386343a4edd76
Mike Abelar <redefiningproductions@gmail.com>
2019-01-01 16:12:39 -0500

    proc file edit

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
-web: gunicorn ohq.wsgi --log-file -
+web: daphne ohq.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker -v2
--- asgi.py
+++ asgi.py
@@ -1,5 +1,6 @@
 import os
-from channels.layers import get_channel_layer
-
+import django
+from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ohq.settings")
-channel_layer = get_channel_layer()
+django.setup()
+application=get_default_application()
