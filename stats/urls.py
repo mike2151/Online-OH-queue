5f2fcb1c75bd934351c0e520285618c77ea8d934
Arjun Guru <arjun.babolat@gmail.com>
2018-12-31 21:01:00 -0500

    dumb bug

--- stats/urls.py
+++ stats/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('/frequent', views.FrequentUserView.as_view())
+    path('frequent/', views.FrequentUserView.as_view())
 ]
