9168f85c9011a5c3111b34739cceab121af60cea
Mike Abelar <redefiningproductions@gmail.com>
2019-01-01 18:05:42 -0500

    attempt to get sockets working

--- ohq/settings.py
+++ ohq/settings.py
@@ -22,7 +22,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static'),
     )
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
