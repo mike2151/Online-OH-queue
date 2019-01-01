61f65e1ed9e3cc456459d37ebf495d866be5b26f
Mike Abelar <redefiningproductions@gmail.com>
2019-01-01 18:07:14 -0500

    attempt to get sockets working

--- ohq/settings.py
+++ ohq/settings.py
@@ -22,10 +22,8 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static'),
     )
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
