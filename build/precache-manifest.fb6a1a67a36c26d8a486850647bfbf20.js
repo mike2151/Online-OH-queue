559cf1a6ff21334498893714c7fb8451b5043a1c
Mike Abelar <redefiningproductions@gmail.com>
2019-01-01 19:40:10 -0500

    attempt to get sockets working

---
+++ build/precache-manifest.fb6a1a67a36c26d8a486850647bfbf20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c32ce3a39459260b47bb",
+    "url": "/static/js/main.c32ce3a3.chunk.js"
+  },
+  {
+    "revision": "0781426c534c9ca7e99b",
+    "url": "/static/js/1.0781426c.chunk.js"
+  },
+  {
+    "revision": "c32ce3a39459260b47bb",
+    "url": "/static/css/main.d7ffca46.chunk.css"
+  },
+  {
+    "revision": "0781426c534c9ca7e99b",
+    "url": "/static/css/1.e86f76b1.chunk.css"
+  },
+  {
+    "revision": "131562cda34924b7e34d034e9480e9f2",
+    "url": "/index.html"
+  }
+];
