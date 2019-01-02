4859a8c665f45c4b6589011b86a5989f16feb9f0
Mike Abelar <redefiningproductions@gmail.com>
2019-01-01 20:31:49 -0500

    attempt to get sockets working

--- frontend/urls.py
+++ frontend/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     re_path(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
