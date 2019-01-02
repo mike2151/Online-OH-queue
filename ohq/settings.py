118bd0ac982fe35570898a64ca4f248e9c209e7f
Mike Abelar <redefiningproductions@gmail.com>
2019-01-01 20:39:21 -0500

    attempt to get sockets working

--- ohq/settings.py
+++ ohq/settings.py
@@ -23,7 +23,6 @@ if DEBUG:
     )
 else:
     STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'build/static'),
     ]
@@ -195,9 +194,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
