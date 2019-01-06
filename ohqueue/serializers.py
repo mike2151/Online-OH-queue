5d1fcb6b6afb6140b0baf52c392e017232be27ff
Mike Abelar <redefiningproductions@gmail.com>
2019-01-06 12:05:59 -0500

    fix failing test cases

--- ohqueue/serializers.py
+++ ohqueue/serializers.py
@@ -8,7 +8,7 @@ class OHQueueSerializer(serializers.ModelSerializer):
     wait_time = serializers.ReadOnlyField()
     class Meta:
         model = models.OHQueue
-        read_only_fields = ('wait_time')
+        read_only_fields = ('wait_time',)
         fields = ('name', 'is_open_extended', 'is_closed', 'is_in_time', 'question_contents', 'wait_time')
     def create(self, validated_data):
         ohqueue =  models.OHQueue.objects.create(**validated_data)
