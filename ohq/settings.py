3d03da178e9981a1f503b2adaa18017bdbad126c
Mike Abelar <redefiningproductions@gmail.com>
2019-01-26 16:17:37 -0500

    remove whitenoise for digitalocean

--- ohq/settings.py
+++ ohq/settings.py
@@ -107,7 +107,6 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
