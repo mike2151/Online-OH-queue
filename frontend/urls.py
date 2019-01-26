07c328fd838048a6c4170c31d087d06851c68318
Mike Abelar <redefiningproductions@gmail.com>
2019-01-26 08:34:54 -0500

    remove password reset

--- frontend/urls.py
+++ frontend/urls.py
@@ -10,20 +10,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('signup/', TemplateView.as_view(template_name='index.html')),
-    path('login/', TemplateView.as_view(template_name='index.html')),
-    path('activated/', TemplateView.as_view(template_name='index.html')),
     path('answer/', TemplateView.as_view(template_name='index.html')),
     path('summary/', TemplateView.as_view(template_name='index.html')),
     path('statistics/', TemplateView.as_view(template_name='index.html')),
     path('<queuename>/ask/', TemplateView.as_view(template_name='index.html')),
     path('<questionid>/edit/', TemplateView.as_view(template_name='index.html')),
-
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        userviews.activate, name='activate'),
-    re_path(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    re_path(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    re_path(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
