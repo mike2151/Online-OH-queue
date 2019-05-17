4549a3ab7118adcc080f7691f069a0e52fdc12cc
Michael Abelar <redefiningproductions@gmail.com>
2019-05-17 07:59:48 -0400

    Added option to export questions as csv

--- questions/admin.py
+++ questions/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Question
+import csv
+from django.http import HttpResponse
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv"]
+
+admin.site.register(Question, QuestionAdmin)
